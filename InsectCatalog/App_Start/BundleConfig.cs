f541ee3b65601e149a783646d69b718802a743ea
Ryan Tanay <ryantanay@newevolution.org>
2015-12-11 13:52:51 -0600

    Created AngularJS script bundle and included it in the base layout

--- InsectCatalog/App_Start/BundleConfig.cs
+++ InsectCatalog/App_Start/BundleConfig.cs
@@ -23,6 +23,10 @@ namespace InsectCatalog
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                      "~/Scripts/angular.min.js",
+                      "~/Scripts/angular-route.min.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
