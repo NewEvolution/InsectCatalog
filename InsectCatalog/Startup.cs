96608d2a3148e78f3f813f28997f2e6dde91fe92
Ryan Tanay <ryantanay@newevolution.org>
2015-12-11 13:27:35 -0600

    Initialized project in Visual Studio

---
+++ InsectCatalog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InsectCatalog.Startup))]
+namespace InsectCatalog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
