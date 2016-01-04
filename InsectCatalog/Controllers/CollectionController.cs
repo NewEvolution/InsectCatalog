8c5fae58fda42aea79878d102691c924449bcbb3
Ryan Tanay <ryantanay@newevolution.org>
2016-01-04 13:33:14 -0600

    Added a controller for the main view, included demo HTML for displaying insects

---
+++ InsectCatalog/Controllers/CollectionController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InsectCatalog.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+
+namespace InsectCatalog.Controllers
+{
+    public class CollectionController : Controller
+    {
+        public InsectRepository Repo { get; set; }
+
+        public CollectionController() : base()
+        {
+            Repo = new InsectRepository();
+        }
+
+        // GET: Collection
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ InsectCatalog/Controllers/InsectController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace InsectCatalog.Controllers
+{
+    public class InsectController : ApiController
+    {
+        // GET: api/Insect
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Insect/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Insect
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Insect/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Insect/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
